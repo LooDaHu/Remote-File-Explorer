d3f2c4613aa54e8fcbf1b878a0b2db921257ceb5
LooDaHu <40339075+LooDaHu@users.noreply.github.com>
2019-08-26 00:51:17 -0700

    Fix found IP address problem

--- tool.py
+++ tool.py
@@ -5,7 +5,7 @@ import re
 def get_local_adr():
     address_set = socket.getaddrinfo(socket.gethostname(), None, family=2)
     for address in address_set:
-        if re.match("192.168.0.", address[4][0]):
+        if re.match("192.168.", address[4][0]):
             local_network_addr = address[4][0]
             return local_network_addr
     return "ADDRESS_NOT_FOUND"
